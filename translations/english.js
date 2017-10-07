92ad25698019eaa00207fb130b960777b9b9109b
Victor Arza <papu.arza@gmail.com>
2017-10-07 12:28:15 +0200

    new changes

---
+++ translations/english.js
@@ -0,0 +1,71 @@
+var english = {
+  menu: {
+    filosofia: "Philosophy",
+    ecommerce: "E-commerce",
+    canalOffline: "Offline Channel",
+    dondeOfrecer: "Where we can offer our services?",
+    clientes: "Clients",
+    contact: "Contact"
+  }
+};
+
+
+Specialists in creating, positioning and maximizing sales channels
+ Creation and optimization of e-commerce channels in different platforms
+ Off-line sales channels, both national and international
+ Development of new or existing products in new markets
+
+E-COMMERCE
+We offer to our client the possibility to create, develop and optimize online
+marketplaces that enter millions of unique users every month:
+AMAZON
+It’s the largest global online sales platform, accessed millions unique users per month just in all
+over the globe.
+Using its platform with Seller Central, being an Amazon supplier with Central Vendor or using a
+joint strategy, you will be able to boost your sales, without neglecting your brand or pricing
+policy.
+MANOMANO
+The biggest marketplace specialized in DIY and gardening. Present in Spain since June 2015, it
+already has more than 70,000 customers, more than 250,000 references in the catalog and
+more than 5 million euros in turnover.
+From an international perspective, since its inception in 2013 as monEchelle, it already has
+more than 850,000 customers, 1 million references and a turnover of more than 90 million
+euros. It is present in France, Belgium, Great Britain, Italy, Germany and Spain.
+
+What does this service allow you?
+Positioning
+Positioning your products in a relevant way in the platform so that your potential sales
+increase as this market grows
+Unique Markets
+Enter unique markets that will allow you to develop your sales globally under global reach
+platforms
+
+Sales Analysis
+Constant analytical information of the platform served to our client to gain more knowledge
+about potential customers identified, sales predictability, stock advice ... etc.
+After-sales management
+After-sales service platform management, in coordination with our client, aligned with their
+online relationship marketing strategies with their customers.
+OFF-LINE CHANNEL
+We create and develop new markets in partnership with our clients under a success approach.
+We offer them access to our local market experts, as quickly as possible and outsourcing its
+costs. We provide to our client an international expansion competitive advantage.
+We offer analytical services and briefings to provide a greater training and knowledge of the
+new market, optimizing our client marketing strategy with greater accuracy. All under a long-
+term business relationship approach.
+Previous Analysis. Development Strategy. Implement Actions. Results Measurement. Correction
+Errors
+WHERE WE CAN OFFER OUR SERVICES?
+We align ourselves with the marketing development strategy of our client in the markets we
+work on, advising always on new proposals to improve their positioning in the country and
+increase market share.
+WHAT IS OUR PHILOSOPHY?
+From PADECO GLOBAL, we are committed to provide our customers with a global, transparent
+and agile market, where they can easily manage and develop their sales with us.
+Our main objective is to be a nexus with the client current commercial network, working in
+tandem with the goal of maximizing its revenues and margins with its actual current structure.
+In this way, we achieve higher sales with the same resources.
+OUR CLIENTS
+We help to sell consolidated companies in their sectors, as well as SMEs and Start-Ups
+
+Specialized in sales channels creation and optimization for companies.
