5450beffa79507df35cedb8dd930e4f480b2292a
Victor Arza <papu.arza@gmail.com>
2017-10-09 06:26:52 +0800

    langugae added

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,13 +1,37 @@
 $(document).ready(function(){
-  $( ".arrow-description" ).click(function(e) {
-    // $(this).parent().next().toggle();
-  });
-
-
-
+      let language = "";
+      checkLanguage();
+      function checkLanguage() {
+        var userLang = navigator.language || navigator.userLanguage;
+        currentLanguage = userLang.substring(0,2);
+        if (currentLanguage === "es") {
+          language = "spanish";
+        } else {
+          language = "english";
+          $("#selected-language").html('<img src="./images/usa.svg" alt=""><p>English</p>')
+          updateLanguage();
+        }
+      }
+      $( ".arrow-description" ).click(function(e) {
+        // $(this).parent().next().toggle();
+      });
+      $(".language-selector").click(function(e) {
+        $("#selected-language").toggle();
+        var currentStyle = $("#menu-language")[0].style.display;
+        if(currentStyle == "flex") {
+          $("#menu-language").css("display", "none");
+        } else {
+          $("#menu-language").css("display", "flex");
+        }
+      });
+      $("#menu-language").click(function(e) {
+        $("#selected-language").html($($(e.target).parent()[0]).html());
+        language = $($(e.target).parent()[0]).attr("id");
+        updateLanguage();
+      });
       $('.parallax-2').parallax();
       $('.carousel').carousel();
-    $('select').material_select();
+      $('select').material_select();
       $('.modal').modal({
         endingTop: "15%",
       });
@@ -15,6 +39,52 @@ $(document).ready(function(){
       setInterval(function () {
         $('.carousel').carousel('next');
       }, 2000);
+
+      function updateLanguage() {
+        if(language === "english") {
+          $('.footer-text').html(english.footer.text);
+          $('#menu-filosofia').html(english.menu.filosofia);
+          $('#menu-canal').html(english.menu.canalOffline);
+          $('#menu-where').html(english.menu.dondeOfrecer);
+          $('#menu-clientes').html(english.menu.clientes);
+          $('#menu-contactar').html(english.menu.contact);
+          $('#header-text').html('Specialists<br><strong><span id="typed">creating</span></strong><br> companies sales channel.');
+          messageText = english.landingHeader.array;
+          $('#header-button').html(english.landingHeader.button);
+          $('#feature-1').html(english.features.feature1);
+          $('#feature-2').html(english.features.feature2);
+          $('#feature-3').html(english.features.feature3);
+          $('#ecommerce-description').html(english.ecommerce.text);
+          $('#amazon-text').html(english.ecommerce.amazon);
+          $('#manomano-text').html(english.ecommerce.manomano);
+          $('#service-features-title').html(english.ecommerce.question);
+          $('#service-features-title-1').html(english.ecommerce.features[0].title);
+          $('#service-features-title-2').html(english.ecommerce.features[1].title);
+          $('#service-features-title-3').html(english.ecommerce.features[2].title);
+          $('#service-features-title-4').html(english.ecommerce.features[3].title);
+          $('#service-features-description-1').html(english.ecommerce.features[0].description);
+          $('#service-features-description-2').html(english.ecommerce.features[1].description);
+          $('#service-features-description-3').html(english.ecommerce.features[2].description);
+          $('#service-features-description-4').html(english.ecommerce.features[3].description);
+          $('#traditional-title').html(english.offline.title);
+          $('#traditional-description-1').html(english.offline.description1);
+          $('#traditional-description-2').html(english.offline.description2);
+          $('#traditional-step-1').html(english.offline.steps[0]);
+          $('#traditional-step-2').html(english.offline.steps[1]);
+          $('#traditional-step-3').html(english.offline.steps[2]);
+          $('#traditional-step-4').html(english.offline.steps[3]);
+          $('#traditional-step-5').html(english.offline.steps[4]);
+          $('#where-title').html(english.world.title);
+          $('#where-text').html(english.world.description);
+          $('#filosofia-title').html(english.philosophy.title);
+          $('#filosofia-description-1').html(english.philosophy.description1);
+          $('#filosofia-description-2').html(english.philosophy.description2);
+          $('#clients-title').html(english.clients.title);
+          $('#clients-text').html(english.clients.description);
+        } else {
+          location.reload();
+        }
+      };
 
       currentIndex = 0;
   messageText = ['optimizar', 'crear', 'posicionar', 'maximizar'];
--- translations/english.js
+++ translations/english.js
@@ -1,25 +1,27 @@
+
 var english = {
   menu: {
     filosofia: "Philosophy",
     ecommerce: "E-commerce",
     canalOffline: "Offline Channel",
-    dondeOfrecer: "Where we can offer our services?",
+    dondeOfrecer: "Where to find us?",
     clientes: "Clients",
     contact: "Contact"
   },
   landingHeader: {
     text: "Specialists in sales channels",
-    array: ["creating", "positioning", "maximizing"]
+    array: ["creating", "positioning", "maximizing"],
+    button: "CHECK OUR SOLUTIONS!"
   },
   features: {
-    feature1:"Creation and optimization of e-commerce channels in different platforms",
-    feature2: "Off-line sales channels, both national and international",
-    feature3: "Development of new or existing products in new markets",
+    feature1:"<strong>Creation and optimization</strong> of e-commerce channels in different platforms",
+    feature2: "<strong>Off-line sales</strong> <br>channels, both <br>national and international",
+    feature3: "Development of new or existing products in <strong>new markets</strong>",
   },
   ecommerce: {
-    text: "We offer to our client the possibility to create, develop and optimize online marketplaces that enter millions of unique users every month",
-    amazon: "It’s the largest global online sales platform, accessed millions unique users per month just in all over the globe. Using its platform with Seller Central, being an Amazon supplier with Central Vendor or using a joint strategy, you will be able to boost your sales, without neglecting your brand or pricing policy.",
-    manomano: "The biggest marketplace specialized in DIY and gardening. Present in Spain since June 2015, it already has more than 70,000 customers, more than 250,000 references in the catalog and more than 5 million euros in turnover. From an international perspective, since its inception in 2013 as monEchelle, it already has more than 850,000 customers, 1 million references and a turnover of more than 90 million euros. It is present in France, Belgium, Great Britain, Italy, Germany and Spain.",
+    text: "We offer to our client the possibility to <strong>create, develop and optimize</strong> online marketplaces that enter <strong>millions of unique users</strong> every month",
+    amazon: "It’s the <strong>largest global online sales platform</strong>, accessed millions unique users per month<br> just in all over the globe. <br><br>Using its platform with <strong>Seller Central</strong>, being an Amazon supplier with <strong>Central Vendor</strong> or using a joint strategy, you will be able to boost your sales, without neglecting your brand or pricing policy.",
+    manomano: "The <strong>biggest marketplace</strong> specialized in DIY and gardening. Present in Spain since June 2015, it already has <strong>more than 70,000 customers</strong>, more than 250,000 references in the catalog and more than 5 million euros in turnover. From an international perspective, since its inception in 2013 as monEchelle, it already has <strong>more than 850,000 customers</strong>, 1 million references and a turnover of more than 90 million euros. It is present in France, Belgium, Great Britain, Italy, Germany and Spain.",
     question: "What does this service allow you?",
     features: [
       {
@@ -32,35 +34,34 @@ var english = {
       },
       {
         title:"Sales Analysis",
-        description: "Constant analytical information of the platform served to our client to gain more knowledgemabout potential customers identified, sales predictability, stock advice ... etc."
+        description: "Constant analytical information of the platform served to our client to gain knowledgemabout potential customers identified, sales predictability, stock advice...etc."
       },
       {
-        title:"After-sales management",
+        title:"After-sales",
         description: "After-sales service platform management, in coordination with our client, aligned with their online relationship marketing strategies with their customers."
       }
-    ],
+    ]
+  },
     offline: {
       title: "OFF-LINE CHANNEL",
-      description: "We create and develop new markets in partnership with our clients under a success approach. We offer them access to our local market experts, as quickly as possible and outsourcing its costs. We provide to our client an international expansion competitive advantage. We offer analytical services and briefings to provide a greater training and knowledge of the new market, optimizing our client marketing strategy with greater accuracy. All under a long-term business relationship approach.",
-      steps: ["Previous Analysis.", "Development Strategy.", "Implement Actions.", "Results Measurement.", "Correction Errors"]
+      description1: "We <strong>create and develop</strong> new markets in partnership with our clients under a success approach. We offer them access to our <strong>local market experts</strong>, as quickly as possible and outsourcing its costs. We provide to our client an <strong>international expansion competitive advantage.</strong>",
+      description2: "We <strong>offer analytical </strong>services and briefings to provide a greater <strong>training and knowledge</strong> of the new market, optimizing our client marketing strategy with greater accuracy. All under a <strong>long-term business relationship</strong> approach.",
+      steps: ["Previous <br>Analysis.", "Development<br> Strategy.", "Implement<br> Actions.", "Results<br> Measurement.", " Errors<br>Correction."]
     },
     world: {
       title: "WHERE WE CAN OFFER OUR SERVICES?",
-      description: "We align ourselves with the marketing development strategy of our client in the markets we work on, advising always on new proposals to improve their positioning in the country and increase market share."
+      description: "We align ourselves with the <strong>marketing development strategy</strong> of our client in the markets we work on, advising always on new proposals to <strong>improve their positioning in the country and increase market share.</strong>"
     },
     philosophy: {
       title: "WHAT IS OUR PHILOSOPHY?",
-      description: "From PADECO GLOBAL, we are committed to provide our customers with a global, transparent and agile market, where they can easily manage and develop their sales with us. Our main objective is to be a nexus with the client current commercial network, working in tandem with the goal of maximizing its revenues and margins with its actual current structure. In this way, we achieve higher sales with the same resources."
+      description1: "From <strong>PADECO GLOBAL</strong>, we are committed to provide our customers with a <strong>global, transparent and agile market</strong>, where they can easily <strong>manage and develop</strong> their sales with us. ",
+      description2: "Our <strong>main objective</strong> is to be a nexus with the client current commercial network, <strong>working in tandem</strong> with the goal of <strong>maximizing its revenues and margins</strong> with its actual current structure. In this way, we achieve <strong>higher sales with the same resources.</strong>"
+    },
+    clients: {
+      title: "OUR CLIENTS",
+      description: "We help to sell consolidated companies in their sectors, as well as SMEs and Start-Ups"
+    },
+    footer: {
+      text: "Specialized in sales channels creation and optimization for companies."
     }
-  }
 };
-
-
-
-
-
-
-OUR CLIENTS
-We help to sell consolidated companies in their sectors, as well as SMEs and Start-Ups
-
-Specialized in sales channels creation and optimization for companies.
